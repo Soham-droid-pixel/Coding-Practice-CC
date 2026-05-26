66a83a872e08564c5206a4ccaafb0f07d7994d9e
Soham <sohamkalg@gmail.com>
2026-05-26 15:32:37 +0530

    Done

---
+++ insertion_sort.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter size of array"<<endl;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=1;i<n;i++){
+        int current=arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>current){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=current;
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<"/t";
+    }
+    return 0;
+}
---
+++ selection_sort.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+
+    cout << "Enter size of array" << endl;
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements" << endl;
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n - 1; i++){
+
+        int minIndex = i;
+
+        for(int j = i + 1; j < n; j++){
+
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
+            }
+        }
+
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+
+    cout << "Sorted Array: ";
+
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << "\t";
+    }
+
+    return 0;
+}
